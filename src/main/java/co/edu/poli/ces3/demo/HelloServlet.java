9e4879f7c89a7d1d1acc29c8d56e9c0622c9b3b5
Eyder Ruiz <eyder305@gmail.com>
2025-08-29 19:36:33 -0500

    modificacion HelloServlet

--- src/main/java/co/edu/poli/ces3/demo/HelloServlet.java
+++ src/main/java/co/edu/poli/ces3/demo/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hola Desde el PCJIC";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
